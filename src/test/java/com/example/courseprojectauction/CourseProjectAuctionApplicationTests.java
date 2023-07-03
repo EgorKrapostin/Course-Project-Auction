48dae7bdad818ea97014a323a430ed1ede605eac
ПК <ekrapostin@gmail.com>
2023-07-03 17:09:29 +0300

    Initial commit

---
+++ src/test/java/com/example/courseprojectauction/CourseProjectAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseprojectauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
